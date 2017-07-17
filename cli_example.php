b16401854692f6e435da2557b4a648104a879d72
Gaston Nan <nangastonr@gmail.com>
2017-07-16 22:44:11 -0300

    readme y example

--- cli_example.php
+++ cli_example.php
@@ -14,55 +14,57 @@ if( !file_exists( 'conf.php' ) ){
 	die();
 }
 
-include_once('vendor/autoload.php');
+require_once('vendor/autoload.php');
 $conf = include( 'conf.php' );
 
 
 $auth_conf = $conf['wsaa'];
 $biller_conf = $conf['wsfev1'];
 
-/* Servicio de autenticacion */
-$auth = AuthServiceFactory::create( $auth_conf['wsdl'], 
-                                    $auth_conf['end_point'],
-                                    $auth_conf['cert_file_name'],
-                                    $auth_conf['key_file_name'],
-                                    $auth_conf['passprhase']  );
-
-/* Servicio de facturación */            
-$biller = BillerServiceFactory::create( $auth, 
-                                        $biller_conf['wsdl'], 
-                                        $biller_conf['end_point'], 
-                                        $conf['cuit'] );
-
-
-$data = array(
-    'Cuit' => '123456789',
-    'CantReg' => 1,
-    'PtoVta' => $biller_conf['sale_point'], //null para que lo intente obtener el web service
-    'CbteTipo' => 06, //A:01 B:06 C:11 
-    'Concepto' => 2, //servicios
-    'DocTipo' => 80, //80=CUIL
-    'DocNro' => '123456789',
-    'CbteDesde' => null, //para que lo calcule uitlizando el web service 
-    'CbteHasta' => null, //para que lo calcule uitlizando el web service
-    'CbteFch' => date('Ymd'),
-    'ImpNeto' => 0,
-    'ImpTotConc' => 1, 
-    'ImpIVA' => 0,
-    'ImpTrib' => 0,
-    'ImpOpEx' => 0,
-    'ImpTotal' => 1, 
-    'FchServDesde' => date("Ymd"), 
-    'FchServHasta' => date("Ymd"), 
-    'FchVtoPago' => date("Ymd"),
-    'MonId' => 'PES', //PES 
-    'MonCotiz' => 1, //1 
-);
-
-
-//solicita cae y cae_validdate
-
 try {
+
+    /* Servicio de autenticacion */
+    $auth = AuthServiceFactory::create( $auth_conf['wsdl'], 
+                                        $auth_conf['end_point'],
+                                        $auth_conf['cert_file_name'],
+                                        $auth_conf['key_file_name'],
+                                        $auth_conf['passprhase']  );
+
+    /* Servicio de facturación */            
+    $biller = BillerServiceFactory::create( $auth, 
+                                            $biller_conf['wsdl'], 
+                                            $biller_conf['end_point'], 
+                                            $conf['cuit'] );
+
+
+    $data = array(
+        'Cuit' => '123456789',
+        'CantReg' => 1,
+        'PtoVta' => $biller_conf['sale_point'], //null para que lo intente obtener el web service
+        'CbteTipo' => 06, //A:01 B:06 C:11 
+        'Concepto' => 2, //servicios
+        'DocTipo' => 80, //80=CUIL
+        'DocNro' => '123456789',
+        'CbteDesde' => null, //para que lo calcule uitlizando el web service 
+        'CbteHasta' => null, //para que lo calcule uitlizando el web service
+        'CbteFch' => date('Ymd'),
+        'ImpNeto' => 0,
+        'ImpTotConc' => 1, 
+        'ImpIVA' => 0,
+        'ImpTrib' => 0,
+        'ImpOpEx' => 0,
+        'ImpTotal' => 1, 
+        'FchServDesde' => date("Ymd"), 
+        'FchServHasta' => date("Ymd"), 
+        'FchVtoPago' => date("Ymd"),
+        'MonId' => 'PES', //PES 
+        'MonCotiz' => 1, //1 
+    );
+
+
+    //solicita cae y cae_validdate
+
+
 
     var_dump( $biller->requestCAE( $data ) );
     
